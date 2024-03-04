e91284a9d0682c348b494159657462c396749383
Sourabh Sikarwar <sourabh.sikarwar@indmoney.com>
2024-03-04 23:26:04 +0530

    eslint config changes

--- packages/ui/.eslintrc.js
+++ packages/ui/.eslintrc.js
@@ -4,6 +4,8 @@ module.exports = {
   extends: ["@repo/eslint-config/react-internal.js"],
   parser: "@typescript-eslint/parser",
   parserOptions: {
-    project: "./tsconfig.lint.json",
+    project: 'tsconfig.json',
+    tsconfigRootDir: __dirname,
+    sourceType: 'module',
   },
 };
